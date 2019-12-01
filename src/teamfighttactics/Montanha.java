8b78324d889c3def3edd2d49ca8369c834fa3443
Wender Lucas Souza <wenderlucas.souza@hotmail.com>
2019-12-01 15:42:07 -0300

    Criação da Factory de elementos.

---
+++ src/teamfighttactics/Montanha.java
@@ -0,0 +1,8 @@
+package teamfighttactics;
+
+public class Montanha implements Elemento {
+    @Override
+    public void aprimoramento(){
+        System.out.println("Concede velocidade de ataque extra.");
+    }
+}
--- src/teamfighttactics/TeamFightTactics.java
+++ src/teamfighttactics/TeamFightTactics.java
@@ -3,7 +3,9 @@ package teamfighttactics;
 public class TeamFightTactics {
 
     public static void main(String[] args) {
-        
-    }
-    
+        //Teste da factory de elementos
+        ElementoFactory elem = new ElementoFactory(); 
+        Elemento e = elem.fabricaElemento("Oceano");
+        e.aprimoramento();
+    }   
 }
